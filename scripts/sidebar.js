de2ccba0fff28c72df242444a27a99ece65753a8
ABARONA <98054848+zashita@users.noreply.github.com>
2022-11-10 15:24:46 +0300

    Add files via upload

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,10 +1,9 @@
 document.getElementsByClassName("header__side-menu-button-pic")[0].onclick = function () {
-    document.body.style.overflow = "hidden";
     document.getElementsByClassName("side-menu")[0].style.width = "100%";
+    setTimeout(()=> document.body.style.overflow = "hidden", 500);
 }
 document.getElementsByClassName("side-menu__close")[0].onclick = function (){
     document.getElementsByClassName("side-menu")[0].style.width = "0";
     document.body.style.overflow = "visible";
-
 }
 
