106a6b2749b570cf9f104dd914a0f909ea8dad11
ABARONA <98054848+zashita@users.noreply.github.com>
2022-11-12 10:39:44 +0300

    Add files via upload

--- scripts/aboutus.js
+++ scripts/aboutus.js
@@ -1,6 +1,12 @@
 
 document.getElementsByClassName("side-menu__link")[2].onclick = function (){
-    document.querySelector("main").style.height = "0";
+    for (let i = 0; i < document.getElementsByName("page").length; i++)
+    {
+        if(document.getElementsByName("page")[i].style.height != "0")
+        {
+            document.getElementsByName("page")[i].style.height = "0";
+        }
+    }
     document.getElementsByClassName("about-us")[0].style.height = "100%";
     document.getElementsByClassName("side-menu")[0].style.width = "0";
     document.getElementsByClassName("side-menu")[0].style.opacity = 0;
